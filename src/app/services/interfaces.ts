375767f1dc8136af55d9de587dc4c2906c92006f
Bryl Magdadaro <magdadarobryl@gmail.com>
2024-09-02 16:06:10 +0800

    Add model for tvShowDetailResult and UpcomingMoviesResult

--- src/app/services/interfaces.ts
+++ src/app/services/interfaces.ts
@@ -39,6 +39,252 @@ export interface MovieResult {
   name: string;
 }
 
+export interface TvShowResult {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: Createdby[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: Lastepisodetoair;
+  name: string;
+  next_episode_to_air: null;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Productioncompany[];
+  production_countries: Productioncountry[];
+  seasons: Season[];
+  spoken_languages: Spokenlanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+  videos: Videos;
+  images: Images;
+  similar: Similar;
+  recommendations: Recommendations;
+  lists: Lists;
+}
+
+export interface UpcomingMoviesResult {
+  dates: Dates;
+  page: number;
+  results: Result[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Result {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number | null;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Dates {
+  maximum: string;
+  minimum: string;
+}
+
+interface Lists {
+  page: number;
+  results: Result4[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Result4 {
+  description: string;
+  favorite_count: number;
+  id: number;
+  item_count: number;
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  poster_path: null | string;
+}
+
+interface Recommendations {
+  page: number;
+  results: Result3[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Result3 {
+  backdrop_path: string;
+  id: number;
+  name: string;
+  original_name: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  adult: boolean;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+}
+
+interface Similar {
+  page: number;
+  results: Result2[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Result2 {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: string;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Images {
+  backdrops: Backdrop[];
+  logos: Logo[];
+  posters: Logo[];
+}
+
+interface Logo {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+interface Backdrop {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: null | string;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+interface Videos {
+  results: Result[];
+}
+
+interface Result {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  published_at: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  id: number | null;
+}
+
+interface Spokenlanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+  vote_average: number;
+}
+
+interface Productioncountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface Productioncompany {
+  id: number;
+  logo_path: null | string;
+  name: string;
+  origin_country: string;
+}
+
+interface Network {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface Lastepisodetoair {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface Createdby {
+  id: number;
+  credit_id: string;
+  name: string;
+  original_name: string;
+  gender: number;
+  profile_path: null | string;
+}
+
 interface Spokenlanguage {
   english_name: string;
   iso_639_1: string;
@@ -47,7 +293,7 @@ interface Spokenlanguage {
 
 interface Productioncompany {
   id: number;
-  logo_path: string;
+  logo_path: string | null;
   name: string;
   origin_country: string;
 }
