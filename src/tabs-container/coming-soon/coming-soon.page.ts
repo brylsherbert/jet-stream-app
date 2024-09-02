9b52b921bc37114774e9516d8fd7895e229a7b32
Bryl Magdadaro <magdadarobryl@gmail.com>
2024-09-02 16:07:08 +0800

    Update coming-soon.page and call API

--- src/tabs-container/coming-soon/coming-soon.page.ts
+++ src/tabs-container/coming-soon/coming-soon.page.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { UpcomingMoviesResult } from './../../app/services/interfaces';
+import { Component, inject } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
+  InfiniteScrollCustomEvent,
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from 'src/app/explore-container/explore-container.component';
+import { catchError, finalize } from 'rxjs';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-coming-soon',
@@ -17,9 +20,50 @@ import { ExploreContainerComponent } from 'src/app/explore-container/explore-con
     IonToolbar,
     IonTitle,
     IonContent,
-    ExploreContainerComponent,
   ],
 })
 export class ComingSoonPage {
-  constructor() {}
+  private movieService = inject(MovieService);
+  public currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public upcomingMovies: UpcomingMoviesResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public spotlight: any;
+  public popularity: any;
+  public movieGenres: any[] = [];
+  activeSlideIndex: number = 0;
+  constructor() {
+    this.getUpcomingMovies();
+  }
+
+  async getUpcomingMovies(event?: InfiniteScrollCustomEvent) {
+    (await this.movieService.getUpcomingMovies(this.currentPage))
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          console.log('Upcoming Movie Results: ', res);
+
+          this.upcomingMovies.push(...res.results);
+          console.log('Results: This Upcoming Movies', this.upcomingMovies);
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+    });
+  }
 }
+
